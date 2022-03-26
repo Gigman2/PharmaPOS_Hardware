d6d64308fe7f4d5774689484db9344fd40d38874
Gigman2 <86406672+Gigman2@users.noreply.github.com>
2022-03-26 08:11:50 +0000

    updated the barcode

--- helpers/barcode.js
+++ helpers/barcode.js
@@ -1,26 +1,44 @@
-const { getDevices, getDevice, UsbScanner } = require('usb-barcode-scanner');
+// const { getDevices, getDevice, UsbScanner } = require('usb-barcode-scanner');
+// const config = require('../config')
+// module.exports = {
+//     devices: () => {
+//         return getDevices()
+//     },
+
+//     device: () => {
+//         let product = config.scanner.productId
+//         let vendor = config.scanner.vendorId
+//         return getDevice(Number(vendor), Number(product));
+//     },
+
+//     scanner: () => {
+//         let scanner = new UsbScanner({
+//             vendorId: config.scanner.vendorId,
+//             productId: config.scanner.productId
+//         });
+
+//         return scanner
+//     }
+// }
+
+const BarcodeScanner = require('barcode-scanner');
 const config = require('../config')
 module.exports = {
     devices: () => {
-        return getDevices()
-    },
-
-    device: () => {
-        let product = config.scanner.productId
-        let vendor = config.scanner.vendorId
-        return getDevice(Number(vendor), Number(product));
+        return BarcodeScanner.device()
     },
 
     scanner: () => {
-        console.log(this.devices)
-        let scanner = new UsbScanner({
+       BarcodeScanner.listen({
             vendorId: config.scanner.vendorId,
             productId: config.scanner.productId
-        });
-
-        return scanner
+       }, function (barcode){
+            return barcode
+       })
     }
 }
+
+
 
 
 
