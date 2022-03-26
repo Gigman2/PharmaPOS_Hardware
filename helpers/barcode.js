f9310d3eb6561c46615ca6ed9f98d9658388a4f0
Gigman2 <86406672+Gigman2@users.noreply.github.com>
2022-03-26 08:29:13 +0000

    updated the barcode

--- helpers/barcode.js
+++ helpers/barcode.js
@@ -1,42 +1,42 @@
-// const { getDevices, getDevice, UsbScanner } = require('usb-barcode-scanner');
-// const config = require('../config')
-// module.exports = {
-//     devices: () => {
-//         return getDevices()
-//     },
-
-//     device: () => {
-//         let product = config.scanner.productId
-//         let vendor = config.scanner.vendorId
-//         return getDevice(Number(vendor), Number(product));
-//     },
-
-//     scanner: () => {
-//         let scanner = new UsbScanner({
-//             vendorId: config.scanner.vendorId,
-//             productId: config.scanner.productId
-//         });
-
-//         return scanner
-//     }
-// }
-
-const BarcodeScanner = require('barcode-scanner');
+const { getDevices, getDevice, UsbScanner } = require('usb-barcode-scanner');
 const config = require('../config')
 module.exports = {
     devices: () => {
-        return BarcodeScanner.device()
+        return getDevices()
+    },
+
+    device: () => {
+        let product = config.scanner.productId
+        let vendor = config.scanner.vendorId
+        return getDevice(Number(vendor), Number(product));
     },
 
     scanner: () => {
-       BarcodeScanner.listen({
+        let scanner = new UsbScanner({
             vendorId: config.scanner.vendorId,
             productId: config.scanner.productId
-       }, function (barcode){
-            return barcode
-       })
+        });
+
+        return scanner
     }
 }
+
+// const BarcodeScanner = require('barcode-scanner');
+// const config = require('../config')
+// module.exports = {
+//     devices: () => {
+//         return BarcodeScanner.device()
+//     },
+
+//     scanner: () => {
+//        BarcodeScanner.listen({
+//             vendorId: config.scanner.vendorId,
+//             productId: config.scanner.productId
+//        }, function (barcode){
+//             return barcode
+//        })
+//     }
+// }
 
 
 
